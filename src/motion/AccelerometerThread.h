18ea409ec2b27c9e2f8f499dd4d2405e23ceccca
kuroanji <33121731+kuroanji@users.noreply.github.com>
2026-02-21 22:43:44 +0400

    Update AccelerometerThread.h

--- src/motion/AccelerometerThread.h
+++ src/motion/AccelerometerThread.h
@@ -7,16 +7,36 @@
 #if !defined(ARCH_STM32WL) && !MESHTASTIC_EXCLUDE_I2C
 
 #include "../concurrency/OSThread.h"
+#include "MotionSensor.h"
+
+// Conditionally include sensor headers only if libraries are available
 #ifdef HAS_BMA423
 #include "BMA423Sensor.h"
 #endif
+#if __has_include(<DFRobot_BMM150.h>)
 #include "BMM150Sensor.h"
+#define HAS_BMM150_LIB 1
+#endif
+#if __has_include(<Rak_BMX160.h>)
 #include "BMX160Sensor.h"
+#define HAS_BMX160_LIB 1
+#endif
+#if __has_include(<ICM_20948.h>)
 #include "ICM20948Sensor.h"
+#define HAS_ICM20948_LIB 1
+#endif
+#if __has_include(<Adafruit_LIS3DH.h>)
 #include "LIS3DHSensor.h"
+#define HAS_LIS3DH_LIB 1
+#endif
+#if __has_include(<Adafruit_LSM6DS3TRC.h>)
 #include "LSM6DS3Sensor.h"
+#define HAS_LSM6DS3_LIB 1
+#endif
+#if __has_include(<Adafruit_MPU6050.h>)
 #include "MPU6050Sensor.h"
-#include "MotionSensor.h"
+#define HAS_MPU6050_LIB 1
+#endif
 #ifdef HAS_QMA6100P
 #include "QMA6100PSensor.h"
 #endif
@@ -88,29 +108,41 @@ class AccelerometerThread : public concurrency::OSThread
             sensor = new BMA423Sensor(device);
             break;
 #endif
+#ifdef HAS_MPU6050_LIB
         case ScanI2C::DeviceType::MPU6050:
             sensor = new MPU6050Sensor(device);
             break;
+#endif
+#ifdef HAS_BMX160_LIB
         case ScanI2C::DeviceType::BMX160:
             sensor = new BMX160Sensor(device);
             break;
+#endif
+#ifdef HAS_LIS3DH_LIB
         case ScanI2C::DeviceType::LIS3DH:
             sensor = new LIS3DHSensor(device);
             break;
+#endif
+#ifdef HAS_LSM6DS3_LIB
         case ScanI2C::DeviceType::LSM6DS3:
             sensor = new LSM6DS3Sensor(device);
             break;
+#endif
 #ifdef HAS_STK8XXX
         case ScanI2C::DeviceType::STK8BAXX:
             sensor = new STK8XXXSensor(device);
             break;
 #endif
+#ifdef HAS_ICM20948_LIB
         case ScanI2C::DeviceType::ICM20948:
             sensor = new ICM20948Sensor(device);
             break;
+#endif
+#ifdef HAS_BMM150_LIB
         case ScanI2C::DeviceType::BMM150:
             sensor = new BMM150Sensor(device);
             break;
+#endif
 #ifdef HAS_QMA6100P
         case ScanI2C::DeviceType::QMA6100P:
             sensor = new QMA6100PSensor(device);
